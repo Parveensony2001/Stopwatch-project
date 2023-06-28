37b8a4cf678429f09db8211e2755ace17c602cb3
Praveen Soni <128714481+Parveensony2001@users.noreply.github.com>
2023-06-29 00:55:40 +0530

    script.js

--- script.js
+++ script.js
@@ -1,41 +1,56 @@
-$(document).ready(function() {
-  $('#studentForm').submit(function(event) {
-    event.preventDefault();
-    let name = $("#name").val();
-    let age = $("#age").val();
-    let marks = $("#marks").val();
-    if (name==false || age==false|| marks==false) {
-            alert("PLEASE FILL ALL BLANCK ")
-            return
-    }
-    
-      if (name && age && marks) {
-    let listItem = $('<li>').text('Name: ' + name + ', Age: ' + age + ', Marks: ' + marks);
 
-    $('#studentList').append(listItem);
+document.addEventListener("DOMContentLoaded",function(){
+const start = document.getElementById("start");
+const reset = document.getElementById("reset");
+const stop = document.getElementById("stop");
+let second = '00';
+let minute = '00';
+let houre = '00';
+let hr = document.getElementById("hr");
+let min  = document.getElementById("min");
+let sec = document.getElementById("sec");
+let setInterval1;
 
-    $("#name").val('');
-    $("#age").val('');
-    $("#marks").val('');
-    }
-  });
+function starttimer() {
+  second++;
+  if(second<9){
+  sec.innerHTML = '0'+second;
+}
+  if(second>9){
+  sec.innerHTML = second;
+}
+  if(second>59){
+    minute++;
+    min.innerHTML='0'+minute;
+    second = 0;
+    sec.innerHTML = '0'+0;
+  }
+  if(minute>9){
+    min.innerHTML = minute;
+  }
+  if(minute>59){
+    houre++;
+    hr.innerHTML = '0'+houre;
+    minute=0;
+    min.innerHTML='0'+0;
+  }
+  if(houre>9){
+    hr.innerHTML=houre;
+  }
+}
+start.addEventListener("click",function(){
+  setInterval1 = setInterval(starttimer);
+})
+stop.addEventListener("click",function(){
+  clearInterval(setInterval1);
+})
+reset.addEventListener("click",function(){
+  clearInterval(setInterval1);
+   second = '00';
+   minute = '00';
+   houre = '00';
+   sec.innerHTML="00";
+   min.innerHTML="00";
+   hr.innerHTML="00";
+})
 });
-/*
-$(document).ready(function() {
-  $('#studentForm').submit(function(event) {
-    event.preventDefault();
-    
-    let name = $("#name").val();
-    let age = $("#age").val();
-    let marks = $("#marks").val();
-    
-    if (name && age && marks) {
-      let listItem = $('<li>').text('Name: ' + name + ', Age: ' + age + ', Marks: ' + marks);
-      $('#studentList').append(listItem);
-      
-      $("#name").val('');
-      $("#age").val('');
-      $("#marks").val('');
-    }
-  });
-});*/
